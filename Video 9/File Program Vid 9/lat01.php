03215ba5e122d1a93271ba68d1e69a2b28a492c6
Wildanthedev <habsyismart@gmail.com>
2022-12-04 09:22:17 +0700

    File Program phpsmk
    File Program Video Playlist PHP untuk siswa SMK

---
+++ Video 9/File Program Vid 9/lat01.php
@@ -0,0 +1,18 @@
+p<?php 
+
+    echo "Belajar PHP";
+
+    echo '<br>';
+
+    echo 'Saya Siswa SMK';
+
+    echo '<h1 style="background-color:red"> Belajar PHP itu Susah </h1>
+        <p style="background-color:blue"> Saya suka belajar Program</p>
+    ';
+
+    echo 'Saya Belajar'.' PHP'.'<br>';
+
+    echo 2020 + 2;
+
+?>
+
